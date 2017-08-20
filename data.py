aa535a88014b6d8e5f57ec5673b2c7f270b8e47a
CokeFung <naruebetcm@gmail.com>
2017-08-20 16:19:41 +0700

    myProfile

--- data.py
+++ data.py
@@ -1,10 +1,10 @@
 data = {
-    'name': 'James Tiberius Kirk',
-    'nickname': 'Jim',
-    'image': 'kirk.jpg',
+    'name': 'Fung K. Phadejtaku',
+    'nickname': 'Fung',
+    'image': 'minions.gif',
     'greeting_msg': 'Hi, there!',
     'about': """
-        James Tiberius "Jim" Kirk was a male Human Starfleet officer in the 23rd century. He was arguably one of the most famous and highly decorated starship captains in the history of Starfleet. As the commanding officer of the Constitution-class starships USS Enterprise and USS Enterprise-A, Kirk served United Federation of Planets interests as an explorer, soldier, time-traveler, and diplomat.
+        Fungkiat Phadejtaku "Fung" , call me Fung.
     """,
     'work': {
         'position': 'Starfleet officer',
